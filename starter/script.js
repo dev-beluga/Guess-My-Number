1b418b79fa1e9dd679f71b00f1c7f2e1c23d4222
Israel Fitsum <fafisru@gmail.com>
2023-01-07 17:03:29 +0300

    Guess my Number

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
