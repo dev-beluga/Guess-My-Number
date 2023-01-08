773f8c9f87d3c5a129d3e7f5ccc26fea67e9f55b
Israel Fitsum <fafisru@gmail.com>
2023-01-08 12:17:04 +0300

    guess my number

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,56 @@
 'use strict';
 
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  //When no input is passed
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️  No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  }
+  //When the guessed number is greater than the secret number
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' 📈 Too High';
+      document.querySelector('.score').textContent = score;
+      score--;
+    } else if (score === 1) {
+      document.querySelector('.message').textContent = ' Last Chance ';
+      document.querySelector('.score').textContent = score;
+      score--;
+    } else {
+      document.querySelector('.message').textContent = ' 💣 Game Over 💣';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //When the guessed number is lesser than the secret number
+  else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' 📉 Too Low';
+      document.querySelector('.score').textContent = score;
+      score--;
+    } else if (score === 1) {
+      document.querySelector('.message').textContent = ' Last Chance ';
+      document.querySelector('.score').textContent = score;
+      score--;
+    } else {
+      document.querySelector('.message').textContent = ' 💣 Game Over 💣';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
